4afeb061363c2de5b67763fc8b1a1c6f314f69da
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-14 21:24:54 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,6 +1,12 @@
 <?php
+$objConnect = mysql_connect("localhost","root","") or die("Error Connect to Database");
+$objDB = mysql_select_db("arduino");
+$strSQL = "SELECT * FROM temp";
+$objQuery = mysql_query($strSQL) or die ("Error Query [".$strSQL."]");
+?>
 
- 
+
+<?php
 $strAccessToken = "sSDv7p584jZ76zRoS3qc1n2VgpPeCRWlIsnYKAiYMR8xcWYPtsMjCxMQiegFygBAMiRRxoNuIUlC5I+sGTTShnJiV637tpjiiotwgvgIQKNLan+rfsTb/wl13EiLqUBmgNvv17qw6B2pnt8Xj/LxjAdB04t89/1O/w1cDnyilFU=";
  
 $content = file_get_contents('php://input');
@@ -45,21 +51,4 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
- 
-
-$objConnect = mysql_connect("localhost","root","") or die("Error Connect to Database");
-$objDB = mysql_select_db("arduino");
-$strSQL = "SELECT * FROM temp";
-$objQuery = mysql_query($strSQL) or die ("Error Query [".$strSQL."]");
-
-/*while($objResult = mysql_fetch_array($objQuery))
-{
-
-  <tr>
-    <td><div align="center"><?php echo $objResult["id"];?></div></td>
-    <td align="center"><?php echo $objResult["temp"];?></td>
- <td align="center"><?php echo $objResult["humidity"];?></td>
-    <td align="center"><?php echo $objResult["date"];?></td>
-  </tr>*/
-mysql_close($objConnect);
 ?>
