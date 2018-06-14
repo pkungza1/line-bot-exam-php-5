625e5841dea3429ebebe2d80467ec327800f6929
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-14 21:09:41 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,4 +1,34 @@
 <?php
+$objConnect = mysql_connect("localhost","root","") or die("Error Connect to Database");
+$objDB = mysql_select_db("arduino");
+$strSQL = "SELECT * FROM temp";
+$objQuery = mysql_query($strSQL) or die ("Error Query [".$strSQL."]");
+?>
+<table width="600" border="1" align="center">
+  <tr>
+    <th width="91"> <div align="center">ID </div></th>
+    <th width="98"> <div align="center">TEMP </div></th>
+ <th width="98"> <div align="center">Humidity </div></th>
+    <th width="198"> <div align="center">DATE </div></th>
+  
+  </tr>
+<?php
+while($objResult = mysql_fetch_array($objQuery))
+{
+?>
+  <tr>
+    <td><div align="center"><?php echo $objResult["id"];?></div></td>
+    <td align="center"><?php echo $objResult["temp"];?></td>
+ <td align="center"><?php echo $objResult["humidity"];?></td>
+    <td align="center"><?php echo $objResult["date"];?></td>
+  </tr>
+<?php
+}
+?>
+</table>
+<?php
+mysql_close($objConnect);
+///////////////////////////////////////////////////////////////***////////////////////////////////////////
  
 $strAccessToken = "sSDv7p584jZ76zRoS3qc1n2VgpPeCRWlIsnYKAiYMR8xcWYPtsMjCxMQiegFygBAMiRRxoNuIUlC5I+sGTTShnJiV637tpjiiotwgvgIQKNLan+rfsTb/wl13EiLqUBmgNvv17qw6B2pnt8Xj/LxjAdB04t89/1O/w1cDnyilFU=";
  
